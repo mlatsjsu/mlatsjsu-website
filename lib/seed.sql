0bef24b699e6289e192aec6cdd943a056c466f44
ahmadgaz <gazali.contact@gmail.com>
2024-08-13 13:32:26 -0700

    added triggers for position resequencing

--- lib/seed.sql
+++ lib/seed.sql
@@ -15,7 +15,6 @@ CREATE TABLE IF NOT EXISTS spotlights (
   title VARCHAR(255) NOT NULL,
   images TEXT[] NOT NULL,
   pos NUMERIC NOT NULL DEFAULT nextval('spotlights_seq'),
-  UNIQUE(pos)
 );
 
 CREATE SEQUENCE IF NOT EXISTS board_seq;
@@ -26,7 +25,6 @@ CREATE TABLE IF NOT EXISTS board (
   linkedin VARCHAR(255) NOT NULL,
   image TEXT NOT NULL,
   pos NUMERIC NOT NULL DEFAULT nextval('board_seq'),
-  UNIQUE(pos)
 );
 
 CREATE SEQUENCE IF NOT EXISTS projects_seq;
@@ -36,7 +34,6 @@ CREATE TABLE IF NOT EXISTS projects (
   description TEXT NOT NULL,
   image TEXT NOT NULL,
   pos NUMERIC NOT NULL DEFAULT nextval('projects_seq'),
-  UNIQUE(pos)
 );
 
 CREATE SEQUENCE IF NOT EXISTS learning_resources_seq;
@@ -45,5 +42,92 @@ CREATE TABLE IF NOT EXISTS learning_resources (
   title VARCHAR(255) NOT NULL,
   link VARCHAR(255) NOT NULL,
   pos NUMERIC NOT NULL DEFAULT nextval('learning_resources_seq'),
-  UNIQUE(pos)
 );
+
+CREATE OR REPLACE FUNCTION resequence_spotlights_pos()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF LENGTH(NEW.pos::TEXT) > 12 THEN
+        WITH Ordered AS (
+            SELECT id, ROW_NUMBER() OVER (ORDER BY pos) AS NewPos
+            FROM spotlights
+        )
+        UPDATE spotlights s
+        SET pos = o.NewPos
+        FROM Ordered o
+        WHERE s.id = o.id;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER check_spotlights_pos_after_update
+AFTER UPDATE ON spotlights
+FOR EACH ROW
+EXECUTE FUNCTION resequence_spotlights_pos();
+
+CREATE OR REPLACE FUNCTION resequence_board_pos()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF LENGTH(NEW.pos::TEXT) > 12 THEN
+        WITH Ordered AS (
+            SELECT id, ROW_NUMBER() OVER (ORDER BY pos) AS NewPos
+            FROM board
+        )
+        UPDATE board b
+        SET pos = o.NewPos
+        FROM Ordered o
+        WHERE b.id = o.id;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER check_board_pos_after_update
+AFTER UPDATE ON board
+FOR EACH ROW
+EXECUTE FUNCTION resequence_board_pos();
+
+CREATE OR REPLACE FUNCTION resequence_projects_pos()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF LENGTH(NEW.pos::TEXT) > 12 THEN
+        WITH Ordered AS (
+            SELECT id, ROW_NUMBER() OVER (ORDER BY pos) AS NewPos
+            FROM projects
+        )
+        UPDATE projects p
+        SET pos = o.NewPos
+        FROM Ordered o
+        WHERE p.id = o.id;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER check_projects_pos_after_update
+AFTER UPDATE ON projects
+FOR EACH ROW
+EXECUTE FUNCTION resequence_projects_pos();
+
+CREATE OR REPLACE FUNCTION resequence_learning_resources_pos()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF LENGTH(NEW.pos::TEXT) > 12 THEN
+        WITH Ordered AS (
+            SELECT id, ROW_NUMBER() OVER (ORDER BY pos) AS NewPos
+            FROM learning_resources
+        )
+        UPDATE learning_resources lr
+        SET pos = o.NewPos
+        FROM Ordered o
+        WHERE lr.id = o.id;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER check_learning_resources_pos_after_update
+AFTER UPDATE ON learning_resources
+FOR EACH ROW
+EXECUTE FUNCTION resequence_learning_resources_pos();
