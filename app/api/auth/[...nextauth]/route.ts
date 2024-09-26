b73f4b1af67221e0c3de2d589a9105aa9c53338c
ahmadgaz <gazali.contact@gmail.com>
2024-09-26 16:13:44 -0700

    removed invalid export authOptions from route

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,6 @@
 import NextAuth from 'next-auth/next';
-import { authOptions as options } from '@/lib/auth-options';
+import { authOptions } from '@/lib/auth-options';
 
-export const authOptions = options;
-
-const handler = NextAuth(options);
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
