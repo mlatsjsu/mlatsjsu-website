a49939152a2e5ae0ef8f6e5bfbb0560c90883a41
Naya Singhania <naya.singhania@gmail.com>
2024-10-02 18:16:26 -0700

    add scaffolding for btn component

---
+++ components/atoms/btn/component.tsx
@@ -0,0 +1,9 @@
+export const Btn: React.FC = ({
+  }) => {
+    return (
+        <div>
+            
+        </div>
+    );
+  };
+  
---
+++ components/atoms/btn/index.ts
@@ -0,0 +1,2 @@
+export { Btn as default } from './component';
+export * from './component';
