67d97825076b17b4f0d4d187cde759d6868d3004
Naya Singhania <naya.singhania@gmail.com>
2024-10-02 18:23:08 -0700

    add code structure for component.tsx

--- components/atoms/btn/component.tsx
+++ components/atoms/btn/component.tsx
@@ -1,9 +1,29 @@
-export const Btn: React.FC = ({
-  }) => {
-    return (
-        <div>
-            
-        </div>
-    );
-  };
-  
+import { cn } from '@/lib/cn';
+
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant: 'primary' | 'secondary';
+  children: React.ReactNode;
+}
+
+export const Btn: React.FC<Props> = ({
+  variant,
+  children,
+  className,
+  ...props
+}) => {
+  return (
+    <button
+      className={cn(
+        '',
+        {
+          '': variant === 'primary',
+          '': variant === 'secondary',
+        },
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
