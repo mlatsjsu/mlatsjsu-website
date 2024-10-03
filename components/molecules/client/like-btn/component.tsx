f1fe7b94b976140cd9dd6b5440179bf8b8f3be31
PatrickTle924 <72108073+PatrickTle924@users.noreply.github.com>
2024-10-03 11:58:44 -0700

    Implement Switching of Icon based on Like Status
    Thumbs up icon lights up orange if Liked

--- components/molecules/client/like-btn/component.tsx
+++ components/molecules/client/like-btn/component.tsx
@@ -3,6 +3,7 @@
 import { Btn } from '@/components/atoms/btn'; //imports atom btn
 
 import thumbsIcon from '@/assets/hand-thumb-up.svg'; //changed the googleicon to the thumbs icon 
+import orangeThumbsIcon from '@/assets/hand-thumb-up-orange.svg';
 //TODO: download and place svg for the thumbs icon
 import Image from 'next/image';
 import React from 'react';//needed?
@@ -13,10 +14,12 @@ export const LikeBtn: React.FC<Props> = ({ ...props }) => { //kept component/fun
     
     const [liked,setLiked] = React.useState<boolean>(false);
     const [likeCount, setLikeCount] = React.useState<number>(0);
+    const [thumbs, setThumbs] = React.useState<string>(thumbsIcon);
     
     function likeHandler() {
         liked ? setLiked(false) : setLiked(true);
         liked ? setLikeCount(likeCount => likeCount-1) : setLikeCount(likeCount => likeCount+1);
+        liked ? setThumbs(thumbsIcon) : setThumbs(orangeThumbsIcon);
     }
 
     return (
@@ -26,6 +29,6 @@ export const LikeBtn: React.FC<Props> = ({ ...props }) => { //kept component/fun
         }
         */
 
-        <Btn {...props} onClick={likeHandler}> <Image src = {thumbsIcon} alt = "thumbsup"/> {likeCount} {liked} </Btn>
+        <Btn {...props} onClick={likeHandler}> <Image src = {thumbs} alt = "thumbsup"/> {likeCount} {liked} </Btn>
     )
 };
