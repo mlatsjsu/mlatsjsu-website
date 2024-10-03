d3948938ab9f962ea2087a9240c801efa30509d7
PatrickTle924 <72108073+PatrickTle924@users.noreply.github.com>
2024-10-02 21:43:10 -0700

    Create Like-btn within Molecules folder
    Added Like-btn's component and index file. Have written out skeleton
    of the button. Heavily based on Auth-btn.

---
+++ components/molecules/client/like-btn/component.tsx
@@ -0,0 +1,19 @@
+import { Btn } from '@/components/atoms/btn'; //imports atom btn
+
+import thumbsIcon from '@/assets/google-icon.svg'; //changed the googleicon to the thumbs icon 
+//TODO: download and place svg for the thumbs icon
+import Image from 'next/image';
+
+interface Props extends React.ComponentProps<typeof Btn>{} //removed omits
+
+export const LikeBtn: React.FC<Props> = ({ ...props }) => { //kept component/function def same from Authbtn
+    return (
+        /*
+        function clickHandler() {
+            setLiked();
+        }
+        */
+
+        <Btn {...props}> Like </Btn>
+    )
+};
