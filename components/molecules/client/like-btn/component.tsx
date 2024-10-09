09ce70d77db8a561e33acf813fa67e837731c9f4
PatrickTle924 <72108073+PatrickTle924@users.noreply.github.com>
2024-10-09 12:50:00 -0700

    Add Proper SVG and Fix Text Color Changes
    Imported proper SVG from @heroicons. Changed cn utility to use the
    correct css class. Removed thumbsup and like state since uneeded.
    Minor code cleanup.

--- components/molecules/client/like-btn/component.tsx
+++ components/molecules/client/like-btn/component.tsx
@@ -1,38 +1,32 @@
-"use client"; //needed?
-import { cn } from '@/lib/cn'; //import tailwind
+"use client"; 
+import { cn } from '@/lib/cn'; //import tailwind utility
 import { Btn } from '@/components/atoms/btn'; //imports atom btn
-
-import thumbsIcon from '@/assets/hand-thumb-up.svg'; //changed the googleicon to the thumbs icon 
-import orangeThumbsIcon from '@/assets/hand-thumb-up-orange.svg';
-//TODO: download and place svg for the thumbs icon
-import Image from 'next/image';
-import React from 'react';//needed?
+import { HandThumbUpIcon } from '@heroicons/react/24/solid'; //import button svg
+import React from 'react';
 
 interface Props extends React.ComponentProps<typeof Btn>{
-} //removed omits
+}
 
 export const LikeBtn: React.FC<Props> = ({className, children, variant, ...props }) => { //kept component/function def same from Authbtn    
 
     const [liked,setLiked] = React.useState<boolean>(false);
     const [likeCount, setLikeCount] = React.useState<number>(0);
-    const [thumbs, setThumbs] = React.useState<string>(thumbsIcon);
     
     function likeHandler() {
         liked ? setLiked(false) : setLiked(true);
         liked ? setLikeCount(likeCount => likeCount-1) : setLikeCount(likeCount => likeCount+1);
-        liked ? setThumbs(thumbsIcon) : setThumbs(orangeThumbsIcon);
     }
 
     return (
         <Btn variant={'secondary'}
             className={cn(
                 {
-                    'text-orange-500': liked,
+                    'text-light-primary': liked,
                     '': !liked,
                 },
             className,
             )}
-            onClick={likeHandler}> <Image src = {thumbs} alt = "thumbsup"/> {likeCount} {liked} 
+            onClick={likeHandler}> <HandThumbUpIcon className="h-6 w-6 text-current"/> {likeCount} {liked} 
         </Btn>
     )
 };
