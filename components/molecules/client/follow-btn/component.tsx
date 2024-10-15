7de90d83ac1bc40ccd26b7da8cd11598e430dc2e
Naya Singhania <naya.singhania@gmail.com>
2024-10-14 21:30:00 -0700

    update follow button to display based off state - initlal state set with prop

--- components/molecules/client/follow-btn/component.tsx
+++ components/molecules/client/follow-btn/component.tsx
@@ -1,26 +1,39 @@
+"use client"
 import { Btn } from '@/components/atoms/btn';
 import { cn } from '@/lib/cn';
+import { useState } from 'react';
 
 interface Props
   extends Omit<
     React.ComponentProps<typeof Btn>,
     'variant' | 'children' | 'href' | 'onClick'
   > {
-    variant: 'follow' | 'unfollow' | 'following';
+    followed?: boolean
   }
 
-export const FollowBtn: React.FC<Props> = ({variant, className, ...props}) => (
-    <Btn 
+export const FollowBtn: React.FC<Props> = ({className, followed=false, ...props}) => {
+  const [following, setFollowing] = useState(followed);
+  const [hover, setHover] = useState(false);
+
+  return (
+  <Btn 
     variant="secondary"
     className={cn(
         {
-            'text-light-primary': variant === 'follow',
-            'text-light-text': variant === 'following',
-            "text-[#CF0000]": variant === "unfollow",
+            'text-light-text hover:text-[#CF0000]': following,
+            'text-light-primary': !following,
         },
         className,
     )}
+    onClick={() => setFollowing(!following)}
+    onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
     >
-      {variant.charAt(0).toUpperCase() + variant.slice(1)}
+      {!hover && !following && 'Follow'}
+      {hover && !following && 'Follow'}
+      {!hover && following && 'Following'}
+      {hover && following && 'Unfollow'}
+
       </Btn>
 )
+}
