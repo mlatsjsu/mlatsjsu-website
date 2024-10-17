70da827e13d02e432352400a00e9f929dd90718a
PatrickTle924 <72108073+PatrickTle924@users.noreply.github.com>
2024-10-16 17:22:17 -0700

    Undo Updates to packages,Delete Unnecessary Assets

--- components/molecules/client/like-btn/component.tsx
+++ components/molecules/client/like-btn/component.tsx
@@ -1,32 +1,43 @@
-"use client"; 
+'use client';
 import { cn } from '@/lib/cn'; //import tailwind utility
 import { Btn } from '@/components/atoms/btn'; //imports atom btn
 import { HandThumbUpIcon } from '@heroicons/react/24/solid'; //import button svg
 import React from 'react';
 
-interface Props extends React.ComponentProps<typeof Btn>{
-}
+interface Props extends React.ComponentProps<typeof Btn> {}
 
-export const LikeBtn: React.FC<Props> = ({className, children, variant, ...props }) => { //kept component/function def same from Authbtn    
+export const LikeBtn: React.FC<Props> = ({
+  className,
+  children,
+  variant,
+  ...props
+}) => {
+  //kept component/function def same from Authbtn
 
-    const [liked,setLiked] = React.useState<boolean>(false);
-    const [likeCount, setLikeCount] = React.useState<number>(0);
-    
-    function likeHandler() {
-        liked ? setLiked(false) : setLiked(true);
-        liked ? setLikeCount(likeCount => likeCount-1) : setLikeCount(likeCount => likeCount+1);
-    }
+  const [liked, setLiked] = React.useState<boolean>(false);
+  const [likeCount, setLikeCount] = React.useState<number>(0);
 
-    return (
-        <Btn variant={'secondary'}
-            className={cn(
-                {
-                    'text-light-primary': liked,
-                    '': !liked,
-                },
-            className,
-            )}
-            onClick={likeHandler}> <HandThumbUpIcon className="h-6 w-6 text-current"/> {likeCount} {liked} 
-        </Btn>
-    )
+  function likeHandler() {
+    liked ? setLiked(false) : setLiked(true);
+    liked
+      ? setLikeCount((likeCount) => likeCount - 1)
+      : setLikeCount((likeCount) => likeCount + 1);
+  }
+
+  return (
+    <Btn
+      variant={'secondary'}
+      className={cn(
+        {
+          'text-light-primary': liked,
+          '': !liked,
+        },
+        className,
+      )}
+      onClick={likeHandler}
+    >
+      {' '}
+      <HandThumbUpIcon className="text-current h-6 w-6" /> {likeCount} {liked}
+    </Btn>
+  );
 };
