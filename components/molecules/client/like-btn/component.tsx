28f7f39c25d77a775d737ff2af456e6a5cad56b0
PatrickTle924 <72108073+PatrickTle924@users.noreply.github.com>
2024-10-30 15:51:00 -0700

    Add Omit to Remove Unnecessary Props

--- components/molecules/client/like-btn/component.tsx
+++ components/molecules/client/like-btn/component.tsx
@@ -4,14 +4,12 @@ import { Btn } from '@/components/atoms/btn'; //imports atom btn
 import { HandThumbUpIcon } from '@heroicons/react/24/solid'; //import button svg
 import React from 'react';
 
-interface Props extends React.ComponentProps<typeof Btn> {}
+interface Props
+  extends Omit<React.ComponentProps<typeof Btn>, 'variant' | 'children'> {}
 
-export const LikeBtn: React.FC<Props> = ({
-  className,
-  children,
-  variant,
-  ...props
-}) => {
+//interface Props extends React.ComponentProps<typeof Btn> {}
+
+export const LikeBtn: React.FC<Props> = ({ className, ...props }) => {
   //kept component/function def same from Authbtn
 
   const [liked, setLiked] = React.useState<boolean>(false);
