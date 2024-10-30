c9d7d64debca229a96f804004058596eae872094
PatrickTle924 <72108073+PatrickTle924@users.noreply.github.com>
2024-10-30 15:27:28 -0700

    Add Input Box and React State

--- components/molecules/comment-textarea/component.tsx
+++ components/molecules/comment-textarea/component.tsx
@@ -10,15 +10,26 @@ interface CTAProps {
 
 export const CommentTextArea: React.FC<CTAProps> = ({}) => {
   const [sent, setSent] = React.useState<boolean>(false);
+  const [txt, setTxt] = React.useState('Add your thoughts...');
 
   function sendHandler() {
     sent ? setSent(false) : setSent(true); //handles button to send data
   }
 
+  function writeHandler(_event: any) {
+    setTxt(_event.target.value);
+    console.log(_event.target.value);
+  }
+
   return (
     <div className="text-light h-141px w-862px m-sm flex flex-col rounded-sm bg-[rgb(var(--color-light-neutral-gray)/0.25)]">
       <div className="m-sm"> Slot in Small User component here</div>
-      Hi
+      <input
+        className="m-sm border-none bg-[rgb(var(--color-light-neutral-gray)/0)] text-light-neutral-gray outline-none"
+        type="text"
+        value={txt}
+        onChange={writeHandler}
+      ></input>
       <div className="m-sm flex justify-end">
         <Btn variant={'primary'} className="text-light-primary">
           {' '}
