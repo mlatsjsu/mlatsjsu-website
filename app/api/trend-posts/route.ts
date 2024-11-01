cd1245845c4fd3e39af0364fe5c1b179e9bd2d1c
PatrickTle924 <72108073+PatrickTle924@users.noreply.github.com>
2024-11-01 16:05:06 -0700

    Add Pagination and Return Page Data

--- app/api/trend-posts/route.ts
+++ app/api/trend-posts/route.ts
@@ -4,18 +4,27 @@ export async function GET(req: Request) {
   try {
     const url = new URL(req.url);
     const lim = parseInt(url.searchParams.get('limit') || '5');
+    const page = parseInt(url.searchParams.get('page') || '0'); //added for pagination
 
     //Query
-    const queryText =
-      'SELECT * FROM posts P ORDER BY P.like_count DESC LIMIT $1;';
-    const queryParams = [lim];
-    const { rows } = await pool.query(queryText, queryParams);
+    const query_text =
+      'SELECT * FROM posts P ORDER BY P.like_count DESC LIMIT $1 OFFSET $2;';
+    const query_params = [lim, page * lim];
+    const { rows } = await pool.query(query_text, query_params);
 
     if (rows.length == 0) {
       return Response.json({ message: 'No Posts Available' }, { status: 404 });
     }
 
-    return Response.json({ posts: rows });
+    const q_text = 'SELECT COUNT(*) FROM posts P';
+    const { rows: row_count } = await pool.query(q_text, []);
+    console.log(row_count);
+
+    return Response.json({
+      posts: rows,
+      page: page,
+      total_pages: Math.ceil(row_count[0].count / lim),
+    });
   } catch (error) {
     if (error instanceof Error) {
       return Response.json({ error: error.message }, { status: 500 });
