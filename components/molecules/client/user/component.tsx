4f189152d0fb2812bd81e65d1021da7933792644
Beaumont Yin <beaumontyin@gmail.com>
2024-11-14 15:59:42 -0800

    Adjusted outer div sizes for UserComponent and UserSmallComponent

--- components/molecules/client/user/component.tsx
+++ components/molecules/client/user/component.tsx
@@ -19,31 +19,31 @@ const UserComponent: React.FC<UserProps> = ({
   avatarUrl,
 }) => {
   return (
-    <div className="flex items-center justify-between w-[460px] h-[89px] gap-md p-4 border border-light-neutral-gray rounded-md">
+    <div className="flex items-center justify-between w-full h-full gap-md p-4 border border-light-neutral-gray rounded-md">
       <div className="flex items-center gap-md">
         {/* Image Box with Fixed Size and Ellipse Shape */}
-        <div className="w-[50px] h-[50px] rounded-full overflow-hidden flex-shrink-0">
+        <div className="w-full h-full rounded-full overflow-hidden flex-shrink-0">
           <Image
             src={avatarUrl}
             alt={name}
             width={50}
             height={50}
-            className="w-full h-full object-cover"
+            className="w-full h-full object-cover rounded-lg"
           />
         </div>
-        <div className="flex flex-col justify-center gap-sm w-[329px]">
-          <h2 className="text-[16px] font-normal leading-[25.6px] text-left text-[#2C353A]">
+        <div className="flex flex-col justify-center flex-1">
+          <h2 className="text-h4-mobile text-[#2C353A]">
             {name}
           </h2>
-          <p className="text-[13.33px] font-normal leading-[21.33px] text-left text-[#90999E]">
+          <p className="text-small-lg text-[#90999E]">
             {followersCount} followers
           </p>
-          <p className="text-[13.33px] font-normal leading-[21.33px] text-left text-[#2C353A]">
+          <p className="text-p text-[#2C353A]">
             {bio}
           </p>
         </div>
       </div>
-      <div className="flex items-start w-[49px] h-[42px]">
+      <div className="flex items-start w-fit h-fit">
         <FollowBtn userId={userId} />
       </div>
     </div>
--- components/molecules/client/user/user-small.tsx
+++ components/molecules/client/user/user-small.tsx
@@ -7,20 +7,20 @@ interface UserSmallProps {
 
 const UserSmallComponent: React.FC<UserSmallProps> = ({ avatarUrl, name }) => {
   return (
-    <div className="flex items-center w-[460px] h-[25px] gap-sm">
+    <div className="flex items-center w-full h-full gap-sm">
       {/* Image Box with Ellipse Shape */}
-      <div className="w-[25px] h-[25px] rounded-full overflow-hidden flex-shrink-0">
+      <div className="w-fit h-fit rounded-full overflow-hidden flex-shrink-0">
         <Image
           src={avatarUrl}
           alt={name}
           width={25}
           height={25}
-          className="object-cover w-full h-full"
+          className="object-cover w-full h-full rounded-lg"
         />
       </div>
       {/* Name Box */}
-      <div className="w-[427px] h-[21px] flex items-center">
-      <span className="text-[13.33px] font-normal leading-[21.33px] text-[var(--light---text, #2C353A)]">
+      <div className="w-full h-fit flex items-center">
+      <span className="text-small-lg text-light-text">
         {name}
       </span>
 
