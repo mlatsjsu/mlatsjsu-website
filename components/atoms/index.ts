9f532a79bde8c6c6fc21ac28495eb29d47a531de
Sai <71161930+cywlol@users.noreply.github.com>
2024-11-15 18:13:42 -0800

    remove text

--- components/atoms/index.ts
+++ components/atoms/index.ts
@@ -3,9 +3,6 @@ export * from './vertical-line';
 export * from './link-btn';
 export * from './card';
 export * from './tag';
-<<<<<<< HEAD
 export * from './popover';
 export * from './btn';
-=======
 export * from './file-input';
->>>>>>> 41b38d3b10d20f39a49a6772f352ea37ce469e04
