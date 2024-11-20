656cb33c2387b129a6af42c738bb9f9771e4b233
Patrick Le <patricktle924@gmail.com>
2024-11-20 14:26:08 -0800

    Swap To Correct Figma Design, Unfinished

--- components/molecules/comment-textarea/component.tsx
+++ components/molecules/comment-textarea/component.tsx
@@ -10,7 +10,7 @@ interface CTAProps {
 
 export const CommentTextArea: React.FC<CTAProps> = ({}) => {
   const [sent, setSent] = React.useState<boolean>(false);
-  const [txt, setTxt] = React.useState('Add your thoughts...');
+  const [txt, setTxt] = React.useState();
 
   function sendHandler() {
     sent ? setSent(false) : setSent(true); //handles button to send data
@@ -22,6 +22,28 @@ export const CommentTextArea: React.FC<CTAProps> = ({}) => {
   }
 
   return (
+    //redo cta according to figma design
+    <div className="text-light  h-141px w-862px m-sm flex flex-col rounded-sm bg-[rgb(var(--color-light-neutral-gray)/0.25)] ">
+      <div className="m-sm flex gap-10">
+        <img src="nothing.com" alt="insert smaller user component" />
+        <div> name here </div>
+      </div>
+      <textarea
+        className="m-sm border-none bg-[rgb(var(--color-light-neutral-gray)/0)] text-light-neutral-gray outline-none"
+        value={txt}
+        onChange={writeHandler}
+        placeholder="Add your thoughts..."
+      ></textarea>
+      <div className="m-sm flex justify-end">
+        <Btn variant={'primary'} className="text-light-primary">
+          {' '}
+          Send{' '}
+        </Btn>
+      </div>
+    </div>
+
+    /*
+    //wrong text area
     <div className="text-light h-141px w-862px m-sm flex flex-col rounded-sm bg-[rgb(var(--color-light-neutral-gray)/0.25)]">
       <div className="m-sm"> Slot in Small User component here</div>
       <input
@@ -37,5 +59,6 @@ export const CommentTextArea: React.FC<CTAProps> = ({}) => {
         </Btn>
       </div>
     </div>
+    */
   );
 };
