4dbd0b93499dcd6fa460bd5e86c599cf0fe24342
HowardWei123 <howieweiinf@gmail.com>
2025-03-03 23:18:07 -0800

    finished and tested GET reading list
    fixed the SQL commands, returns list of post_ids + number of pages

--- app/api/users/[id]/reading-list/route.ts
+++ app/api/users/[id]/reading-list/route.ts
@@ -7,20 +7,21 @@ export async function GET(
   { params }: { params: { id: string } },
 ) {
   try {
+    // Validate user ID
     const id = params.id;
     if (!id || !/^[0-9]+$/.test(id)) {
         throw new Error('Invalid ID 400');
     }
     const user_id = await getSessionUserId();
+    if (user_id === null ) {
+      throw new Error("User ID not found 404");
+    }
 
-    if (user_id === undefined ) {
+    if (user_id === undefined || user_id !== parseInt(id)) {
       throw new Error("Unauthorized User ID 401");
     }
 
-    if (user_id === null ) {
-      throw new Error("User ID not found 401");
-    }
-    
+    // Retrieve limit and page from the URL
     const url = new URL(req.url);
     const limit: number = parseInt(url.searchParams.get('limit') as string) || 10;
     const page: number = parseInt(url.searchParams.get('page') as string) || 1;
@@ -28,7 +29,7 @@ export async function GET(
 
     // Retrieve reading list for the authenticated user
     const readingList: { rows: { post_id: string }[] } = await pool.query(
-      'SELECT post_id FROM reading_lists WHERE user_id = $1 LIMIT $2 OFFSET $3',
+      'SELECT post_id FROM reading_lists WHERE user_id = $1 LIMIT $2 OFFSET $3;',
       [user_id, limit, offset]
     );
 
@@ -36,8 +37,9 @@ export async function GET(
       throw new Error('Reading list empty 404');
     }
 
+    // Calculate total pages based on the total number of records
     const totalRecords: { rows: { count: string }[] } = await pool.query(
-      'SELECT COUNT(*) FROM reading_list WHERE user_id = $1',
+      'SELECT COUNT(*) FROM reading_lists WHERE user_id = $1;',
       [user_id]
     );
     const totalPages: number = Math.ceil(parseInt(totalRecords.rows[0].count) / limit);
